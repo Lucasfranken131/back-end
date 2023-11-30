792e647ea84e8dabfbe90d4b090f45b3619e47a7
Lucasfranken131 <lucas.franco@aluno.fmpsc.edu.br>
2023-11-30 00:50:20 -0300

    Cadastro

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
 
 async function bootstrap() {
+  const expressApp = express();
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    methods: 'GET, POST, PUT, DELETE',
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
